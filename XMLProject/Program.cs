46ee358461a649b15cb1d306c44294e8d23444bf
FatmahAlqhtany <82486115+FatmahAlqhtany@users.noreply.github.com>
2021-07-01 16:23:08 +0300

    CreateXMLFiles

--- XMLProject/Program.cs
+++ XMLProject/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
-
+using System;
+using System.Xml;
 namespace XMLProject
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          CreateXMLFiles();
+      
         }
+        static void CreateXMLFiles()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            XmlElement root = xmldoc.CreateElement("team");
+            xmldoc.AppendChild(root);
+            XmlElement element1 = xmldoc.CreateElement("member");
+            XmlText text1 = xmldoc.CreateTextNode("Fatimah");
+            XmlAttribute Attribute1 = xmldoc.CreateAttribute("first-name");
+            Attribute1.Value = "Fatimah";
+            root.AppendChild(element1);
+            element1.AppendChild(text1);
+            xmldoc.Save(@"C:\Users\win\Desktop\TeamMembers.xml");
+            Console.WriteLine(xmldoc.InnerText);
+        }
+       
     }
 }
