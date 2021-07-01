c4baeed1c597a03fcaadce9a5477bfc26f66bb9e
Abdulaziz Alasmari <abdulaziz.alasmari@tuwaiq.edu.sa>
2021-07-01 19:52:47 +0300

    refactor: change variables names and method extraction add: add readme file and output xml file

--- XMLProject/Program.cs
+++ XMLProject/Program.cs
@@ -10,56 +10,46 @@ namespace XMLProject
         {
             Thread writeThread = new Thread(CreateXMLFiles);
             Thread readThread = new Thread(ReadXMLFile);
-            
             writeThread.Start();
             writeThread.Join();
-
             readThread.Start();
             readThread.Join();
         }
 
-        private static XmlElement CreateXmlElement(XmlDocument xmldoc, string name)
-        {
-            return xmldoc.CreateElement(name);
-        }
-
-        private static XmlText CreateXmlText(XmlDocument xmldoc, string name)
-        {
-            return xmldoc.CreateTextNode(name);
-        }
-
+        private static void SetColor(ConsoleColor textColor) => Console.ForegroundColor = textColor;
+        private static XmlElement CreateXmlElement(XmlDocument xmldoc, string name) => xmldoc.CreateElement(name);
+        private static XmlText CreateXmlText(XmlDocument xmldoc, string name) => xmldoc.CreateTextNode(name);
         static void CreateXMLFiles()
         {
             XmlDocument xmldoc = new XmlDocument();
-            XmlElement root = xmldoc.CreateElement("team");
-            xmldoc.AppendChild(root);
-
-            var element = CreateXmlElement(xmldoc, "leader");
-            root.AppendChild(element).AppendChild(CreateXmlText(xmldoc, "Ahmed Alzubaidi"));
-            var element2 = CreateXmlElement(xmldoc, "member");
-            root.AppendChild(element2).AppendChild(CreateXmlText(xmldoc, "Fatimah Alqhtany"));
-            var element3 = CreateXmlElement(xmldoc, "member");
-            root.AppendChild(element3).AppendChild(CreateXmlText(xmldoc, "Maryam Alraddadi"));
-            var element4 = CreateXmlElement(xmldoc, "member");
-            root.AppendChild(element4).AppendChild(CreateXmlText(xmldoc, "Abdulaziz Alasmari"));
-            var element5 = CreateXmlElement(xmldoc, "member");
-            root.AppendChild(element5).AppendChild(CreateXmlText(xmldoc, "Mansour Aldundur"));
-            
+            XmlElement team = xmldoc.CreateElement("team");
+            xmldoc.AppendChild(team);
+            var leader = CreateXmlElement(xmldoc, "leader");
+            team.AppendChild(leader).AppendChild(CreateXmlText(xmldoc, "Ahmed Alzubaidi"));
+            var member1 = CreateXmlElement(xmldoc, "member");
+            team.AppendChild(member1).AppendChild(CreateXmlText(xmldoc, "Fatimah Alqhtany"));
+            var member2 = CreateXmlElement(xmldoc, "member");
+            team.AppendChild(member2).AppendChild(CreateXmlText(xmldoc, "Maryam Alraddadi"));
+            var member3 = CreateXmlElement(xmldoc, "member");
+            team.AppendChild(member3).AppendChild(CreateXmlText(xmldoc, "Abdulaziz Alasmari"));
+            var member4 = CreateXmlElement(xmldoc, "member");
+            team.AppendChild(member4).AppendChild(CreateXmlText(xmldoc, "Mansour Aldundur"));
             xmldoc.Save(@"team.xml");
-            Console.WriteLine(xmldoc.InnerText);
         }
+
         static void ReadXMLFile()
         {
             XmlDocument xmldoc = new XmlDocument();
             xmldoc.Load(@"team.xml");
+            Console.Clear();
             foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
             {
-                string element = node.Name;
-                Console.WriteLine(element + ": ");
-                string text = node.InnerText;
-                Console.WriteLine(text);
+                SetColor(ConsoleColor.Cyan);
+                Console.WriteLine(" {0} : ",node.Name);
+                SetColor(ConsoleColor.Yellow);
+                Console.WriteLine("  {0} ",node.InnerText);
             }
         }
-       
+
     }
 }
---
+++ XMLProject/README.md
@@ -0,0 +1,25 @@
+<h4 align="center">XML PROJECT</h4>
+
+<p align="center">
+  Create and read xml files and using threads
+</p>
+
+<p align="center">
+  <img src="output.png" alt="demo"/></img>
+</p>
+
+<br/>
+
+## Group Members
+
+> [Ahmed Alzubaidi](https://github.com/AhmedTuwaiq)
+
+> [Fatimah Alqahtani](https://github.com/FatmahAlqhtany)
+
+> [Maryam Alraddadi](https://github.com/maryam-alraddadi)
+
+> [Abdulaziz Alasmari](https://github.com/Abdulaziz-Alasmari)
+
+> [Mansour Aldundur](https://github.com/aldundur-Mansour)
+
+</div>
